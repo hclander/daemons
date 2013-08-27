860740103f3aadeb5880c38c40d89a297a74d72f
Juan Carlos "juanky" Mendez <jcmendez@locatel.biz>
2013-08-27 15:31:59 +0200

    Decodemanager init

--- decodemanager.c
+++ decodemanager.c
@@ -5,4 +5,22 @@
  *      Author: jcmendez
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "lib/mem.h"
+#include "frames.h"
+#include "mydb.h"
+#include "decodemanager.h"
+
+#define T decodemanager_t
+
+struct T {
+	decode_context_t context;
+
+
+};
+
 
--- decodemanager.h
+++ decodemanager.h
@@ -8,6 +8,27 @@
 #ifndef DECODEMANAGER_H_
 #define DECODEMANAGER_H_
 
+typedef struct {
+	int    rx_id;
+	int    loc_id;
+	time_t time;
+
+} decode_context_t;
+
+#define T decodemanager_t
+
+typedef T *T;
 
 
+T decodemanager_create();
+void decodemanager_destroy(T *dm);
+
+void decodemanagee_init(T dm);
+
+void decodemanager_finish(T dm);
+
+int decodemanager_decode(T db, void *buff, size_t size, int *count);
+
+
+#undef T
 #endif /* DECODEMANAGER_H_ */
